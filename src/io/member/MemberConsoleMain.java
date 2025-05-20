e17f2083ea2359bea540cf22627a6c4daf4e75c7
hyun <apfhd0257@naver.com>
2025-05-20 18:51:45 +0900

    회원 관리 예제 - DataStream

--- src/io/member/MemberConsoleMain.java
+++ src/io/member/MemberConsoleMain.java
@@ -1,5 +1,6 @@
 package io.member;
 
+import io.member.impl.DataMemberRepository;
 import io.member.impl.FileMemberRepository;
 import io.member.impl.MemoryMemberRepository;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MemberConsoleMain {
-    private static final MemberRepository repository = new FileMemberRepository();
+    private static final MemberRepository repository = new DataMemberRepository();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
